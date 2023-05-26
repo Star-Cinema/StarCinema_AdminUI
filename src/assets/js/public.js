3576f0512bed857a2708c2fb88876fcb5085dc88
Duc Hung <hungklyhongkl@gmail.com>
2023-05-26 13:35:46 +0700

    26 h

--- src/assets/js/public.js
+++ src/assets/js/public.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+//Request API to upload image in to Cloudinary HungTD34
 export const UploadImageAPI = async (file) => {
     const formData = new FormData();
+
+    //Add file in to formData to upload file HungTD34
     formData.append("file", file)
+
+    //Add name of presets config HungTD34
     formData.append("upload_preset", "ml_default")
 
     var data
